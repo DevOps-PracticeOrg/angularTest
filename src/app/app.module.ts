217592d1263687a52e841e21e7577f07511d3031
Kou Naka <123windam@gmail.com>
2020-09-25 17:43:04 +0900

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+//パーツ
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+//ページ
+import { TopComponent } from './top/top.component';
+import { AtelierComponent } from './atelier/atelier.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    AtelierComponent,
+    UserComponent,
+    HeaderComponent,
+    FooterComponent,
+    TopComponent,
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: TopComponent },
+      { path: 'user/:user_id', component: UserComponent },
+      { path: 'atelier/:atelier_id', component: AtelierComponent },
+    ])
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
